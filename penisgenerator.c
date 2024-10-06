04597b4e5bad0e9459f5c79f018d380cd0d09cd5
soupy <soupy@soupy.fish>
2024-10-06 23:39:07 +0100

    add comments

--- penisgenerator.c
+++ penisgenerator.c
@@ -4,22 +4,21 @@
 
 int main()
 {
+  // initialise rand
+  srand(time(NULL));
 
   char *penisSize[] = {"small\0", "medium\0", "large\0", "strange\0"};
 
-  srand(time(NULL));
-
-  // mfw anvil
+  // bottom surgery
   printf("AGHHHHHHHhhhhhhhhhhhhhhhhhhhhhhhhHHHHH\n");
   printf("my peanus\n");
   printf("oooooo my pronis\n");
-
   printf("the penis has been destroyed. please generate another\n");
 
+  // generate a number between 0 and 4
   int randSize = rand() % 4;
 
-  // printf("%d\n", randSize);
-  // puts(penisSize[randSize]);
+  //print the penis size
   printf("penis size = %s\n", penisSize[randSize]);
 
   return 0;
