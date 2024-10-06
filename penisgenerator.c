d206246bb1f2f08eab7f358da0b183ac51239cb3
soupy <soupy@soupy.fish>
2024-10-06 22:10:13 +0100

    create penis generator

---
+++ penisgenerator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+  int penisSize = 4;
+
+  //mfw anvil 
+  printf("AGHHHHHHHhhhhhhhhhhhhhhhhhhhhhhhhHHHHH \n");
+  printf("my peanus \n");
+  printf("oooooo my pronis\n");
+
+  printf("the penis has been destroyed. please generate another\n");
+  printf("penis size = %d\n", penisSize);
+
+  return 0;
+
+  
+}
